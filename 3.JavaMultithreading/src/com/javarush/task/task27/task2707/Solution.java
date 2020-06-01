dd2e968a40ff1ac5d77966a311d20a6f4c0bd861
Dmytro-V <wvaskod@gmail.com>
2020-06-01 23:13:15 +0200

    chap3, level7 lesson6

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2707/Solution.java
@@ -0,0 +1,50 @@
+package com.javarush.task.task27.task2707;
+
+/* 
+Определяем порядок захвата монитора
+*/
+public class Solution {
+    public void someMethodWithSynchronizedBlocks(Object obj1, Object obj2) {
+        synchronized (obj1) {
+            synchronized (obj2) {
+                System.out.println(obj1 + " " + obj2);
+            }
+        }
+    }
+
+    public static boolean isLockOrderNormal(final Solution solution, final Object o1, final Object o2) throws Exception {
+        //do something here
+
+        Thread methodThread = new Thread() {
+            @Override
+            public void run() {
+                solution.someMethodWithSynchronizedBlocks(o1, o2);
+            }
+        };
+        Thread threadB = new Thread() {
+            @Override
+            public void run() {
+                synchronized (o2) {
+                }
+            }
+        };
+
+        synchronized (o1) {
+            methodThread.start();
+            Thread.sleep(500);
+            threadB.start();
+            Thread.sleep(500);
+            return !threadB.isAlive();
+
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        final Solution solution = new Solution();
+        final Object o1 = new Object();
+        final Object o2 = new Object();
+
+        System.out.println(isLockOrderNormal(solution, o1, o2));
+    }
+}
