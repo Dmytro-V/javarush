3a0b1b75b54ff7e905a06596291898751eddf9cf
dim <wvaskod@gmail.com>
2020-06-11 18:05:48 +0300

    level7, archiver 10

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,8 +1,13 @@
 package com.javarush.task.task31.task3110;
 
+import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+
+import java.io.FileInputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -16,21 +21,46 @@ public class ZipFileManager {
 
     public void createZip(Path source) throws Exception{
 
+        if (!Files.exists(zipFile.getParent())) {
+            Files.createDirectory(zipFile.getParent());
+        }
         try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile))) {
 
-            ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
+            if (Files.isRegularFile(source)) {
 
+                addNewZipEntry(zipOutputStream, source.getParent(), source.getFileName());
+
+            } else if (Files.isDirectory(source)) {
+
+                FileManager fileManager = new FileManager(source);
+                List<Path> fileNames = fileManager.getFileList();
+                for (Path path : fileNames) {
+                    addNewZipEntry(zipOutputStream, source, path);
+                }
+
+            } else {
+                throw new PathIsNotFoundException();
+            }
+        }
+    }
+
+    private void addNewZipEntry(ZipOutputStream zipOutputStream, Path filePath, Path fileName) throws Exception {
+
+        try (InputStream inputStream = Files.newInputStream(filePath.resolve(fileName))) {
+
+            ZipEntry zipEntry = new ZipEntry(fileName.toString());
             zipOutputStream.putNextEntry(zipEntry);
 
-            try (InputStream inputStream = Files.newInputStream(source)) {
-
-                while (inputStream.available() != 0) {
-                    zipOutputStream.write(inputStream.read());
-                }
-            }
-            zipOutputStream.closeEntry();
+            copyData(inputStream, zipOutputStream);
         }
+        zipOutputStream.closeEntry();
 
+    }
+
+    private void copyData(InputStream in, OutputStream out) throws Exception {
+        while (in.available() != 0) {
+            out.write(in.read());
+        }
     }
 
 }
