ce9e97cd25d9c0eaf905b060ce16fb258465b395
dim <wvaskod@gmail.com>
2020-06-28 22:08:58 +0200

    task 11

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -2,8 +2,10 @@ package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
 import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
+import com.javarush.task.task32.task3209.listeners.UndoListener;
 
 import javax.swing.*;
+import javax.swing.undo.UndoManager;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,12 +17,19 @@ public class View extends JFrame implements ActionListener {
     private JTextPane htmlTextPane = new JTextPane();
     private JEditorPane plainTextPane = new JEditorPane();
 
+    private UndoManager undoManager = new UndoManager();
+    private UndoListener undoListener = new UndoListener(undoManager);
+
     public Controller getController() {
         return controller;
     }
 
     public void setController(Controller controller) {
         this.controller = controller;
+    }
+
+    public UndoListener getUndoListener() {
+        return undoListener;
     }
 
     @Override
@@ -86,10 +95,30 @@ public class View extends JFrame implements ActionListener {
     }
 
     public boolean canUndo() {
-        return false;
+        return undoManager.canUndo();
     }
 
     public boolean canRedo() {
-        return false;
+        return undoManager.canRedo();
+    }
+
+    public void undo() {
+        try {
+            undoManager.undo();
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
+    public void redo() {
+        try {
+            undoManager.redo();
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
+    public void resetUndo() {
+        undoManager.discardAllEdits();
     }
 }
