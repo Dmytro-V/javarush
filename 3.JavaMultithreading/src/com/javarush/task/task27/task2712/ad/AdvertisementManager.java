0a993d71509fb15f8d8635097749c620661ab2b0
dim <wvaskod@gmail.com>
2020-07-12 22:55:40 +0200

    task 8

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
@@ -1,8 +1,19 @@
 package com.javarush.task.task27.task2712.ad;
 
-public class AdvertisementManager {
+import com.javarush.task.task27.task2712.ConsoleHelper;
 
+public class AdvertisementManager {
+    
     private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
 
+    private int timeSeconds;
+
+    public AdvertisementManager(int timeSeconds) {
+        this.timeSeconds = timeSeconds;
+    }
+
+    public void processVideos() {
+        ConsoleHelper.writeMessage("calling processVideos method");
+    }
 
 }
