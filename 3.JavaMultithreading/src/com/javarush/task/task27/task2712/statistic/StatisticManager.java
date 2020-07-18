2854d01840efb5100dd78a665c73e9d2b0bc044c
dim <wvaskod@gmail.com>
2020-07-18 22:32:01 +0200

    task 13

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -1,10 +1,18 @@
 package com.javarush.task.task27.task2712.statistic;
 
 import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
+import com.javarush.task.task27.task2712.statistic.event.EventType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class StatisticManager {
 
     private final static StatisticManager statisticManager = new StatisticManager();
+    private StatisticStorage statisticStorage = new StatisticStorage();
+
 
     private StatisticManager() {
 
@@ -17,5 +25,19 @@ public class StatisticManager {
     public void register(EventDataRow data) {
 
     }
+
+    private class StatisticStorage {
+
+        private Map<EventType, List<EventDataRow>> storage;
+
+        public StatisticStorage() {
+            storage = new HashMap<>();
+            for (EventType eventType : EventType.values()) {
+                storage.put(eventType, new ArrayList<EventDataRow>());
+            }
+
+        }
+    }
+
 
 }
