1c211ef5bf55ed597886a21751a8da77c78e060c
dim <wvaskod@gmail.com>
2020-08-08 21:12:39 +0200

    task 5

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,11 +1,13 @@
 package com.javarush.task.task35.task3513;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
 
 public class Model {
     private static final int FIELD_WIDTH = 4;
     private Tile[][] gameTiles;
+    int score;
+    int maxTile;
 
     public Model() {
         resetGameTiles();
@@ -18,6 +20,8 @@ public class Model {
                 gameTiles[i][j] = new Tile();
             }
         }
+        this.score = 0;
+        this.maxTile = 0;
         addTile();
         addTile();
     }
@@ -42,5 +46,32 @@ public class Model {
             }
         }
         return emptyTiles;
+    }
+
+    private void compressTiles(Tile[] tiles) {
+
+        for (int j = 0; j < tiles.length - 1; j++) {
+            for (int i = 0; i < tiles.length - 1; i++) {
+                if (tiles[i].isEmpty() && !tiles[i + 1].isEmpty()) {
+                    tiles[i].value = tiles[i + 1].value;
+                    tiles[i + 1].value = 0;
+                }
+            }
+        }
+    }
+
+    private void mergeTiles(Tile[] tiles) {
+        for (int i = 0; i < tiles.length - 1; i++) {
+            if (tiles[i].value == tiles[i + 1].value) {
+                tiles[i].value +=  tiles[i + 1].value;
+                tiles[i + 1].value = 0;
+
+                this.score += tiles[i].value;
+                if (tiles[i].value > this.maxTile) {
+                    maxTile = tiles[i].value;
+                }
+            }
+        }
+        compressTiles(tiles);
     }
 }
